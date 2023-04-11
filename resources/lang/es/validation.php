19e443df82ce038d7ab97e9dae1314a98d2e3399
bomparta <alexisbompart@hotmail.com>
2023-04-11 13:19:40 -0400

    cambio del iioma a los errores

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -31,7 +31,7 @@ return [
     'alpha'                => 'El campo :attribute solo puede contener letras.',
     'alpha_dash'           => 'El campo :attribute solo puede contener letras, números, guiones y guiones bajos.',
     'alpha_num'            => 'El campo :attribute solo puede contener letras y números.',
-    'array'                => 'El campo :attribute debe ser un array.',
+    'array'                => 'El campo :attribute debe ser un arreglo.',
     'before'               => 'El campo :attribute debe ser una fecha anterior a :date.',
     'before_or_equal'      => 'El campo :attribute debe ser una fecha anterior o igual a :date.',
     'between'              => [
