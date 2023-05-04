a06ab823f2c205ff6361c24253f3bb760c4f2b47
bomparta <alexisbompart@hotmail.com>
2023-05-04 10:48:34 -0400

    carnet de trabajo ajustes

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -1093,7 +1093,7 @@ public function destroyfamiliar($id)
             //Recibimos el archivo y lo guardamos en la carpeta storage/app/public
            // $request->file('archivo')->store('public/foto_carnet');
           // dd($request->hasfile('archivo'));
-        //   dd($request->all());
+           dd($request->all());
 
             if($request->hasfile('archivo')):
                 $imagen         = $request->file('archivo');
